93e82b3bef91ae042b9ea1fd5547680934732af7
Anupama-Annepu08 <anupamaannepu8@gmail.com>
2026-08-10 11:35:56 +0530

    feat: add API gateway with Eureka discovery

---
+++ backend/api-gateway/src/test/java/com/ngoconnect/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngoconnect.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
